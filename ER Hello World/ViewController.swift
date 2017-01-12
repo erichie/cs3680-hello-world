12fd7fb5cd726b795202ff4f66a5dfe81bced530
Edward Richards <e4.rich87@gmail.com>
2017-01-12 08:39:42 -0700

    Initial Commit

---
+++ ER Hello World/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ER Hello World
+//
+//  Created by Edward Richards on 1/12/17.
+//  Copyright © 2017 Edward Richards. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ ER Hello WorldTests/ER_Hello_WorldTests.swift
@@ -0,0 +1,36 @@
+//
+//  ER_Hello_WorldTests.swift
+//  ER Hello WorldTests
+//
+//  Created by Edward Richards on 1/12/17.
+//  Copyright © 2017 Edward Richards. All rights reserved.
+//
+
+import XCTest
+@testable import ER_Hello_World
+
+class ER_Hello_WorldTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
