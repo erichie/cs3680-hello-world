52d69fee9a24d11bf09e9a0b25638b43c499d149
Edward Richards <e4.rich87@gmail.com>
2017-01-19 16:57:18 -0700

    initial commit with basic show/hide description functionality

--- ER Hello World/ViewController.swift
+++ ER Hello World/ViewController.swift
@@ -9,17 +9,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let name: String = "Edward"
+    
+    @IBOutlet var gameDescription: UITextView!
+    @IBOutlet var showGameDescriptionButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
 
+        gameDescription.isHidden = true
+        
+        // Two ways to print statements and variables to the console
+        print("Hello \(name)")
+        NSLog(name)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
+    
+    @IBAction func showGameDescription(_sender: UIButton) {
+        if gameDescription.isHidden {
+            showGameDescriptionButton.setTitle("Hide Description", for: UIControlState.normal)
+            gameDescription.isHidden = false
+        }
+        else {
+            showGameDescriptionButton.setTitle("Show Description", for: UIControlState.normal)
+            gameDescription.isHidden = true
+        }
+        
+    }
 }
 
